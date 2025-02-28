7ea79a55fa2a07864766e2686da9af9a2f71a975
severogianluca <gianlucasevero300@gmail.com>
2025-02-28 20:00:55 +0100

    Fear: create ciclo for reverse

--- palindromo/js/app.js
+++ palindromo/js/app.js
@@ -1,1 +1,13 @@
-console.log('Ciao')
+// creo il promt dove l'utente  inserirà la parola
+let insertWord = prompt ('Inserisci una parola!');
+
+// creo una funzione che prende come parametro la parola dell'user
+// col for la ciclerò al contrario e la farò ritornare come una nuova parola
+function checkPalindromo(userWord){
+    let newUserWord='';
+    for(let i = userWord.length -1; i >= 0; i--){
+        newUserWord += userWord[i]
+    }
+    return newUserWord;    
+}
+
