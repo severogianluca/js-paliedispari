f019261a68f6f5ab62ae7ad04af4cf02bdc44b8c
severogianluca <gianlucasevero300@gmail.com>
2025-03-01 03:58:14 +0100

    Feat: creat random number user/computer

--- pari-dispari/js/paroDisparo.js
+++ pari-dispari/js/paroDisparo.js
@@ -1,3 +1,11 @@
 // promt per la scelta dell'utente
 const choose = prompt('Paro o disparo?');
 
+const userNumber = Math.ceil(Math.random() * 5);
+console.log('Il numero dell utente è:' + userNumber );
+
+function computerRandomNumber(){
+    return Math.ceil(Math.random() * 5);
+}
+const computerNumber = computerRandomNumber();
+console.log('Il numero del computer è: ' + computerNumber());
