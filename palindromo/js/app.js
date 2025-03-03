49da6dbe36fb822e5ef43e6a8ecb20dc9bcd7a5c
severogianluca <gianlucasevero300@gmail.com>
2025-03-03 11:24:40 +0100

    fix

--- palindromo/js/app.js
+++ palindromo/js/app.js
@@ -5,7 +5,7 @@ let insertWord = prompt ('Inserisci una parola!');
 // col for la ciclerò al contrario e la farò ritornare come una nuova parola
 // utilizzo toLowerCase per trasformare le parole dell'utente anche quelle maiuscole in minuscolo
 function checkPalindromo(userWord){
-    userWord = userWord.toLowerCase;
+    userWord = userWord.toLowerCase();
     let newUserWord='';
     for(let i = userWord.length -1; i >= 0; i--){
         newUserWord += userWord[i]
@@ -21,7 +21,8 @@ console.log(checkPalindromo(insertWord));
 // creo una condizione dove inserisco la parola dell'utente e lo metto a confronto
 // con la funzione che lo leggerà al contrario 
 // se le parole sono identiche sarà palindromo altrimenti no
-if(insertWord === checkPalindromo(insertWord)){
+// aggiunto il toLowerCase anche nella condizione da parte dell'utente
+if(insertWord.toLocaleLowerCase() === checkPalindromo(insertWord)){
     console.log('la parola è palindroma');
 }else{
     console.log('la parola non è palindroma');
