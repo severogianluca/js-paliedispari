dbdd37b265f915f5ffba0ce5756c7e4f21187f6f
severogianluca <gianlucasevero300@gmail.com>
2025-03-03 11:19:08 +0100

    Fix: utilizzo toLowerCase

--- palindromo/js/app.js
+++ palindromo/js/app.js
@@ -4,6 +4,7 @@ let insertWord = prompt ('Inserisci una parola!');
 // creo una funzione che prende come parametro la parola dell'user
 // col for la ciclerò al contrario e la farò ritornare come una nuova parola
 function checkPalindromo(userWord){
+    userWord = userWord.toLowerCase;
     let newUserWord='';
     for(let i = userWord.length -1; i >= 0; i--){
         newUserWord += userWord[i]
